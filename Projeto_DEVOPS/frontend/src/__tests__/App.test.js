eae58ec56642edfa04ee9246e9250edf3969d1fb
Lucas Vilhena <66475011+Fulanalok@users.noreply.github.com>
2025-05-26 09:59:01 -0300

    apagando o antigo

--- Projeto_DEVOPS/frontend/src/__tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-
-test("Renderiza título de login", () => {
-  render(<App />);
-  const title = screen.getByText(/login/i);
-  expect(title).toBeInTheDocument();
-});
